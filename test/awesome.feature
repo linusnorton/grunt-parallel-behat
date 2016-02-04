9689f0a4f24c01247eae020b3e5661110242dd11
Daniel McDermott <dmcdermott@forumone.com>
2016-02-03 23:18:29 -0800

    Fix to test, now checking actual feature files, updated test based on language change

---
+++ test/awesome.feature
@@ -0,0 +1,4 @@
+Feature: Check that this feature is awesome
+
+  Scenario: As a user I want to visit Google
+    Given I visit "https://google.com"
--- test/lib/BehatTask.js
+++ test/lib/BehatTask.js
@@ -34,7 +34,7 @@ suite('Behat Test', function () {
             fail: {
                 warn: log
             },
-            files: ['awesome.feature', 'brilliant.feature'],
+            files: ['test/awesome.feature', 'test/brilliant.feature'],
             bin: 'behat',
             flags: '',
             maxProcesses: 10000
@@ -46,8 +46,8 @@ suite('Behat Test', function () {
         task.run();
 
         assert.equal(mockExecutor.addTask.callCount, 2);
-        assert.equal(mockExecutor.addTask.args[0][0], 'behat awesome.feature');
-        assert.equal(mockExecutor.addTask.args[1][0], 'behat brilliant.feature');
+        assert.equal(mockExecutor.addTask.args[0][0], 'behat test/awesome.feature');
+        assert.equal(mockExecutor.addTask.args[1][0], 'behat test/brilliant.feature');
         assert.equal(mockExecutor.start.callCount, 1);
     });
 
@@ -63,8 +63,8 @@ suite('Behat Test', function () {
 
     test('provides user feedback', function () {
         stub(mockExecutor, 'start', function () {
-            mockExecutor.callbacks.startedTask.call(task, 'behat awesome.feature');
-            mockExecutor.callbacks.finishedTask.call(task, 'behat awesome.feature', void 0, '3 scenarios (3 passed)\n\n5m15s\n');
+            mockExecutor.callbacks.startedTask.call(task, 'behat test/awesome.feature');
+            mockExecutor.callbacks.finishedTask.call(task, 'behat test/awesome.feature', void 0, '3 scenarios (3 passed)\n\n5m15s\n');
             mockExecutor.callbacks.finished();
         });
 
@@ -72,15 +72,15 @@ suite('Behat Test', function () {
         task.run();
 
         assert.equal(log.callCount, 4);
-        assert.equal(log.args[0][0], 'Found 2 feature file(s). Running 10000 at a time.');
-        assert.equal(log.args[1][0], 'Started: behat awesome.feature');
-        assert.equal(log.args[2][0], 'Completed: awesome.feature - 3 scenarios (3 passed) in 5m15s');
+        assert.equal(log.args[0][0], 'Running 10000 at a time.');
+        assert.equal(log.args[1][0], 'Started: behat test/awesome.feature');
+        assert.equal(log.args[2][0], 'Completed: test/awesome.feature - 3 scenarios (3 passed) in 5m15s');
         assert(log.args[3][0].indexOf('Finished in') > -1);
     });
 
     test('handles timeouts', function () {
         stub(mockExecutor, 'start', function () {
-            mockExecutor.callbacks.finishedTask.call(task, 'behat awesome.feature', {code: 13}, '');
+            mockExecutor.callbacks.finishedTask.call(task, 'behat test/awesome.feature', {code: 13}, '');
         });
 
         mockExecutor.isFinished = stub().returns(false);
@@ -88,36 +88,36 @@ suite('Behat Test', function () {
 
         assert.equal(log.callCount, 2);
         assert.equal(mockExecutor.addTask.callCount, 3);
-        assert.equal(log.args[1][0], 'Timeout: awesome.feature - adding to the back of the queue.');
+        assert.equal(log.args[1][0], 'Timeout: test/awesome.feature - adding to the back of the queue.');
     });
 
     test('handles failed tests', function () {
         stub(mockExecutor, 'start', function () {
-            mockExecutor.callbacks.finishedTask.call(task, 'behat awesome.feature', {code: 1}, '3 scenarios (1 passed, 2 failed)\n\n5m15s\n');
+            mockExecutor.callbacks.finishedTask.call(task, 'behat test/awesome.feature', {code: 1}, '3 scenarios (1 passed, 2 failed)\n\n5m15s\n');
         });
 
         mockExecutor.isFinished = stub().returns(false);
         task.run();
 
         assert.equal(log.callCount, 3);
-        assert.equal(log.args[1][0], 'Failed: awesome.feature - 3 scenarios (1 passed, 2 failed) in 5m15s');
+        assert.equal(log.args[1][0], 'Failed: test/awesome.feature - 3 scenarios (1 passed, 2 failed) in 5m15s');
     });
 
     test('handles unknown errors', function () {
         stub(mockExecutor, 'start', function () {
-            mockExecutor.callbacks.finishedTask.call(task, 'behat awesome.feature', {code: 1000000}, 'ZOMG! I\'m dead!!');
+            mockExecutor.callbacks.finishedTask.call(task, 'behat test/awesome.feature', {code: 1000000}, 'ZOMG! I\'m dead!!');
         });
 
         mockExecutor.isFinished = stub().returns(false);
         task.run();
 
         assert.equal(log.callCount, 2);
-        assert.equal(log.args[1][0], 'Error: awesome.feature - [object Object]ZOMG! I\'m dead!!');
+        assert.equal(log.args[1][0], 'Error: test/awesome.feature - [object Object]ZOMG! I\'m dead!!');
     });
 
     test('adds failed tasks back to the queue if numRetries is specified', function () {
         stub(mockExecutor, 'start', function () {
-            mockExecutor.callbacks.finishedTask.call(task, 'behat awesome.feature', {code: 1}, 'Test failed');
+            mockExecutor.callbacks.finishedTask.call(task, 'behat test/awesome.feature', {code: 1}, 'Test failed');
         });
 
         mockExecutor.isFinished = stub().returns(false);
@@ -134,7 +134,7 @@ suite('Behat Test', function () {
             fail: {
                 warn: log
             },
-            files: ['awesome.feature', 'brilliant.feature'],
+            files: ['test/awesome.feature', 'test/brilliant.feature'],
             bin: 'behat',
             flags: '',
             maxProcesses: 10000,
@@ -142,15 +142,15 @@ suite('Behat Test', function () {
         });
         task.run();
 
-        assert.equal(log.args[1][0], 'Failed: awesome.feature - undefined in undefined');
-        assert.equal(log.args[2][0], 'Retrying: awesome.feature 1 of 2 time(s)');
+        assert.equal(log.args[1][0], 'Failed: test/awesome.feature - undefined in undefined');
+        assert.equal(log.args[2][0], 'Retrying: test/awesome.feature 1 of 2 time(s)');
         assert.equal(log.callCount, 3);
         assert.equal(mockExecutor.addTask.callCount, 3);
     });
 
     test('adds pending tasks back to the queue if numRetries is specified', function () {
         stub(mockExecutor, 'start', function () {
-            mockExecutor.callbacks.finishedTask.call(task, 'behat awesome.feature', {code: 1}, '3 scenarios (1 passed, 2 pending)\n\n5m15s\n');
+            mockExecutor.callbacks.finishedTask.call(task, 'behat test/awesome.feature', {code: 1}, '3 scenarios (1 passed, 2 pending)\n\n5m15s\n');
         });
 
         task = new BehatTask({
@@ -165,7 +165,7 @@ suite('Behat Test', function () {
             fail: {
                 warn: log
             },
-            files: ['awesome.feature', 'brilliant.feature'],
+            files: ['test/awesome.feature', 'test/brilliant.feature'],
             bin: 'behat',
             flags: '',
             maxProcesses: 10000,
@@ -173,8 +173,8 @@ suite('Behat Test', function () {
         });
         task.run();
 
-        assert.equal(log.args[1][0], 'Failed: awesome.feature - 3 scenarios (1 passed, 2 pending) in 5m15s');
-        assert.equal(log.args[2][0], 'Retrying: awesome.feature 1 of 2 time(s)');
+        assert.equal(log.args[1][0], 'Failed: test/awesome.feature - 3 scenarios (1 passed, 2 pending) in 5m15s');
+        assert.equal(log.args[2][0], 'Retrying: test/awesome.feature 1 of 2 time(s)');
         assert.equal(log.callCount, 3);
         assert.equal(mockExecutor.addTask.callCount, 3);
     });
